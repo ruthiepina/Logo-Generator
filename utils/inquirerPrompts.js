ebfbfe565c63bd34247144a211b0b90f5c59b24b
RMP <ruthiepina@gmail.com>
2023-12-25 19:10:42 -0700

    Created prompts for text input, text color, shape type, and shape color for generated SVG logo.

---
+++ utils/inquirerPrompts.js
@@ -0,0 +1,50 @@
+import inquirer from "inquirer";
+
+let shapeTypes = [];
+
+const inquirerPrompts = async () => {
+   const appIntro = `
+---------------------------------------------
+   Welcome to the SVG file generator
+---------------------------------------------
+
+
+Instructions:
+
+1. Understand what each prompt is asking you.
+2. Some prompts will ask you to enter a color keyword or hexadecimal number. 
+   a. Visit https://www.w3.org/wiki/CSS/Properties/color/keywords for a full list of supported CSS color keywords and hexadecimal numbers.
+3. Use the arrow keys to select from the shape type list.
+4. Press enter after every input.
+Your answer will be then automatically received by the application.
+
+Enjoy it!
+
+`;
+   const questions = [
+      {
+         type: "input",
+         name: "logoText",
+         message: "Enter up to 3 characters for your logo text.",
+      },
+      {
+         type: "input",
+         name: "textColor",
+         message: "Enter a color keyword (OR a hexadecimal number) for your logo text color selection.",
+      },
+      {
+         type: "list",
+         name: "shape",
+         message: "Select the shape for your logo.",
+         choices: shapeTypes,
+      },
+      {
+         type: "input",
+         name: "shapeColor",
+         message: "Enter a color keyword (OR a hexadecimal number) for your logo shape color selection.",
+      },
+   ];
+   console.log(appIntro);
+   const answers = await inquirer.prompts(questions);
+   return { answers, shapeTypes };
+};
